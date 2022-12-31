a1a33314c076042343e7744a08ea207cf521d5d6
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 19:39:31 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// background.js
 // Select the element that displays the online status of a contact
 const onlineStatusSelector = '.pane-header .chat-status';
 
@@ -5,20 +6,35 @@ const onlineStatusSelector = '.pane-header .chat-status';
 const audioElement = document.createElement('audio');
 audioElement.src = 'notification.mp3';
 
-// Observe the online status element for changes
-const observer = new MutationObserver(mutations => {
-  // Check if the online status of a contact has changed
-  const onlineStatusChanged = mutations.some(
-    mutation => mutation.attributeName === 'title'
-  );
+// Wait for the online status element to be added to the DOM
+const waitForElement = selector => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      const element = document.querySelector(selector);
+      if (element) {
+        clearInterval(interval);
+        resolve(element);
+      }
+    }, 100);
+  });
+};
 
-  if (onlineStatusChanged) {
-    // Play the notification sound
-    audioElement.play();
-  }
-});
+waitForElement(onlineStatusSelector).then(element => {
+  // Observe the online status element for changes
+  const observer = new MutationObserver(mutations => {
+    // Check if the online status of a contact has changed
+    const onlineStatusChanged = mutations.some(
+      mutation => mutation.attributeName === 'title'
+    );
 
-// Start observing the online status element
-observer.observe(document.querySelector(onlineStatusSelector), {
-  attributes: true
+    if (onlineStatusChanged) {
+      // Play the notification sound
+      audioElement.play();
+    }
+  });
+
+  // Start observing the online status element
+  observer.observe(element, {
+    attributes: true
+  });
 });
