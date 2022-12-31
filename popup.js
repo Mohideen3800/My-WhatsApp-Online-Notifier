256c7aeea231c7e00b4c4f859673899f4f8b3a82
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 20:33:34 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,38 +1,5 @@
 // popup.js
 
-// Wait for the contacts list to be added to the DOM
-const waitForContacts = () => {
-  return new Promise((resolve, reject) => {
-    let timeout = setTimeout(() => {
-      clearInterval(interval);
-      reject(new Error('Timed out waiting for contacts list'));
-    }, 5000); // Set the timeout to 5 seconds
-
-    const interval = setInterval(() => {
-      const contacts = document.querySelectorAll('.pane-header');
-      if (contacts.length > 0) {
-        clearInterval(interval);
-        clearTimeout(timeout);
-        resolve(contacts);
-      }
-    }, 100);
-  });
-};
-
-waitForContacts().then(contacts => {
-  // Get the <select> element
-  const select = document.querySelector('select');
-
-  // Add the first 10 contacts to the dropdown menu
-  contacts.slice(0, 10).forEach(contact => {
-    const name = contact.getAttribute('title');
-    const option = document.createElement('option');
-    option.value = name;
-    option.textContent = name;
-    select.appendChild(option);
-  });
-});
-
 // Select the form element
 const form = document.querySelector('form');
 
@@ -40,7 +7,7 @@ const form = document.querySelector('form');
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  // Get the selected contact
+  // Get the phone number of the selected contact
   const contact = form.elements.contact.value;
 
   // Save the selected contact using the chrome.storage API
