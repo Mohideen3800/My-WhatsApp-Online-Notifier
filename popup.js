83210878b2afefaf54219293ac00b7d13a4022bd
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 20:16:47 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,12 +17,28 @@ waitForContacts().then(contacts => {
   // Get the <select> element
   const select = document.querySelector('select');
 
-  // Add an option for each contact
-  contacts.forEach(contact => {
+  // Add the first 10 contacts to the dropdown menu
+  contacts.slice(0, 10).forEach(contact => {
     const name = contact.getAttribute('title');
     const option = document.createElement('option');
     option.value = name;
     option.textContent = name;
     select.appendChild(option);
+  });
+});
+
+// Select the form element
+const form = document.querySelector('form');
+
+// Handle form submission
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // Get the selected contact
+  const contact = form.elements.contact.value;
+
+  // Save the selected contact using the chrome.storage API
+  chrome.storage.sync.set({ contact }, () => {
+    console.log(`Saved contact: ${contact}`);
   });
 });
