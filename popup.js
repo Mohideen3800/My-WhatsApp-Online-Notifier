df8a049d4b2cd74a14b388f1ed4b3f3df586750d
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 20:55:47 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -13,5 +13,35 @@ form.addEventListener('submit', event => {
   // Save the selected contact using the chrome.storage API
   chrome.storage.sync.set({ contact }, () => {
     console.log(`Saved contact: ${contact}`);
+
+    // Display the selected contact and enable the view activity log button
+    const selectedContactDiv = document.querySelector('#selected-contact');
+    selectedContactDiv.innerHTML = `Selected contact: ${contact}`;
+    const viewActivityLogButton = document.querySelector('#view-activity-log');
+    viewActivityLogButton.disabled = false;
   });
+
+  // Handle errors
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (const key in changes) {
+      const storageChange = changes[key];
+      console.log(
+        'Storage key "%s" in namespace "%s" changed. ' +
+          'Old value was "%s", new value is "%s".',
+        key,
+        namespace,
+        storageChange.oldValue,
+        storageChange.newValue
+      );
+      // Display an error message
+      form.innerHTML = '<p>Error saving contact. Please try again.</p>';
+    }
+  });
+});
+
+// Handle click on the view activity log button
+const viewActivityLogButton = document.querySelector('#view-activity-log');
+viewActivityLogButton.addEventListener('click', () => {
+  // Show the activity log page action for the current tab
+  chrome.pageAction.show(tabs[0].id);
 });
