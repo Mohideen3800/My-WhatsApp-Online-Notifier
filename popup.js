5eb2c833f6a5e9b897515ebaa0eb0ca36120b15a
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 20:03:57 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,28 @@
 // popup.js
-// Select the form element
-const form = document.querySelector('form');
 
-// Handle form submission
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  // Get the selected contact
-  const contact = form.elements.contact.value;
-
-  // Save the selected contact using the chrome.storage API
-  chrome.storage.sync.set({ contact }, () => {
-    console.log(`Saved contact: ${contact}`);
+// Wait for the contacts list to be added to the DOM
+const waitForContacts = () => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      const contacts = document.querySelectorAll('.pane-header');
+      if (contacts.length > 0) {
+        clearInterval(interval);
+        resolve(contacts);
+      }
+    }, 100);
   });
+};
+
+waitForContacts().then(contacts => {
+  // Get the <select> element
+  const select = document.querySelector('select');
+
+  // Add an option for each contact
+  contacts.forEach(contact => {
+    const name = contact.getAttribute('title');
+    const option = document.createElement('option');
+    option.value = name;
+    option.textContent = name;
+    select.appendChild(option);
+  });
+});
