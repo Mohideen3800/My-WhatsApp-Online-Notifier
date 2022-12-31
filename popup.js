70fd72c539a3c493bdf671c95e43d8e7b5de13b2
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 21:07:53 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,47 +1,19 @@
 // popup.js
 
-// Select the form element
-const form = document.querySelector('form');
-
-// Handle form submission
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  // Get the phone number of the selected contact
-  const contact = form.elements.contact.value;
-
-  // Save the selected contact using the chrome.storage API
-  chrome.storage.sync.set({ contact }, () => {
-    console.log(`Saved contact: ${contact}`);
-
-    // Display the selected contact and enable the view activity log button
+// Display the selected contact when it's updated
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'update_selected_contact') {
     const selectedContactDiv = document.querySelector('#selected-contact');
-    selectedContactDiv.innerHTML = `Selected contact: ${contact}`;
-    const viewActivityLogButton = document.querySelector('#view-activity-log');
-    viewActivityLogButton.disabled = false;
-  });
-
-  // Handle errors
-  chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (const key in changes) {
-      const storageChange = changes[key];
-      console.log(
-        'Storage key "%s" in namespace "%s" changed. ' +
-          'Old value was "%s", new value is "%s".',
-        key,
-        namespace,
-        storageChange.oldValue,
-        storageChange.newValue
-      );
-      // Display an error message
-      form.innerHTML = '<p>Error saving contact. Please try again.</p>';
-    }
-  });
+    selectedContactDiv.innerHTML = `Selected contact: ${request.contact}`;
+  }
 });
 
-// Handle click on the view activity log button
-const viewActivityLogButton = document.querySelector('#view-activity-log');
-viewActivityLogButton.addEventListener('click', () => {
-  // Show the activity log page action for the current tab
-  chrome.pageAction.show(tabs[0].id);
+// Get the phone number of the selected contact from storage
+chrome.storage.sync.get(['contact'], result => {
+  const selectedContact = result.contact;
+  if (selectedContact) {
+    // Display the selected contact
+    const selectedContactDiv = document.querySelector('#selected-contact');
+    selectedContactDiv.innerHTML = `Selected contact: ${selectedContact}`;
+  }
 });
