42685628bc5dad60c3c777ddcc299487e206de97
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 19:49:59 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,16 @@
+// popup.js
+// Select the form element
+const form = document.querySelector('form');
+
+// Handle form submission
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // Get the selected contact
+  const contact = form.elements.contact.value;
+
+  // Save the selected contact using the chrome.storage API
+  chrome.storage.sync.set({ contact }, () => {
+    console.log(`Saved contact: ${contact}`);
+  });
+});
