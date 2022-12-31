7b11278fa08a4dad6cb7fd22498fde3b135790e8
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2022-12-31 21:32:18 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,39 @@
 // popup.js
 
-const startTrackingButton = document.querySelector('#start-tracking');
+// Add event listeners for the Start tracking and Stop tracking buttons
+document.getElementById('start-tracking').addEventListener('click', startTracking);
+document.getElementById('stop').addEventListener('click', stopTracking);
 
-startTrackingButton.addEventListener('click', () => {
-  // Get the phone number of the selected contact from the page
-  const selectedContact = document.querySelector('._3CNEp._1f1zm').title;
+// Start tracking the selected contact
+function startTracking() {
+  // Get the phone number from the input field
+  const phoneNumber = document.getElementById('phone-number').value;
 
-  // Save the selected contact to storage
-  chrome.storage.sync.set({ contact: selectedContact });
-});
+  // Save the phone number in storage
+  chrome.storage.sync.set({ contact: phoneNumber }, () => {
+    // Update the UI to show that tracking has started
+    document.getElementById('select-contact').style.display = 'none';
+    document.getElementById('stop-tracking').style.display = 'block';
+    document.getElementById('tracked-contact').textContent = phoneNumber;
+
+    // Send a message to the content script to start tracking the contact
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'startTracking' });
+    });
+  });
+}
+
+// Stop tracking the selected contact
+function stopTracking() {
+  // Clear the phone number from storage
+  chrome.storage.sync.remove('contact', () => {
+    // Update the UI to show that tracking has stopped
+    document.getElementById('select-contact').style.display = 'block';
+    document.getElementById('stop-tracking').style.display = 'none';
+
+    // Send a message to the content script to stop tracking the contact
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'stopTracking' });
+    });
+  });
+}
