66b9e084128055773699ca9b7237b7fb1c60dc32
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2023-01-01 17:11:14 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,19 @@
-// Retrieve the online history for each contact
-chrome.storage.local.get(null, (items) => {
-  for (const key in items) {
-    if (key.startsWith('contact_')) {
-      const contact = key.replace('contact_', '');
-      const history = items[key];
-      // Display the online history for the contact
-      const li = document.createElement('li');
-      li.innerHTML = `${contact}: ${history.online ? 'Online' : 'Offline'}`;
-      document.querySelector('#contacts').appendChild(li);
+// Make sure that the chrome.storage.local object is defined before trying to use it
+if (typeof chrome !== 'undefined' && typeof chrome.storage !== 'undefined' && typeof chrome.storage.local !== 'undefined') {
+  // Retrieve the online history for each contact
+  chrome.storage.local.get(null, (items) => {
+    for (const key in items) {
+      if (key.startsWith('contact_')) {
+        const contact = key.replace('contact_', '');
+        const history = items[key];
+        // Display the online history for the contact
+        const li = document.createElement('li');
+        li.innerHTML = `${contact}: ${history.online ? 'Online' : 'Offline'}`;
+        document.querySelector('#contacts').appendChild(li);
+      }
     }
-  }
-});
+  });
+}
 
 // Navigate to the history page when the link is clicked
 document.querySelector('#history').addEventListener('click', (event) => {
