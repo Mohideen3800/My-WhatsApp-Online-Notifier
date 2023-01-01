3a9fd6d2f37b72ab64f1459158ca25546ed1194c
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2023-01-01 16:42:53 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,19 @@
+// Retrieve the online history for each contact
+chrome.storage.local.get(null, (items) => {
+  for (const key in items) {
+    if (key.startsWith('contact_')) {
+      const contact = key.replace('contact_', '');
+      const history = items[key];
+      // Display the online history for the contact
+      const li = document.createElement('li');
+      li.innerHTML = `${contact}: ${history.online ? 'Online' : 'Offline'}`;
+      document.querySelector('#contacts').appendChild(li);
+    }
+  }
+});
+
+// Navigate to the history page when the link is clicked
+document.querySelector('#history').addEventListener('click', (event) => {
+  event.preventDefault();
+  chrome.tabs.create({ url: 'history.html' });
+});
